643c1217abfb006636d7dc6a71e8a495928eaa88
wavy0 <69266903+wavy0@users.noreply.github.com>
2025-08-21 15:27:02 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-dnf5 install -y steam
+dnf5 install -y tmux steam gamemode gamescope
 
 # Use a COPR Example:
 #
